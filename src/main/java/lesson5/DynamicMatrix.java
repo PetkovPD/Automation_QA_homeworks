37edadb105d1dae872242275db565fef38dbc2c5
PC <petkov.d.petko@gmail.com>
2024-03-09 19:20:42 +0200

    Adding solution for DynamicMatrix task

---
+++ src/main/java/lesson5/DynamicMatrix.java
@@ -0,0 +1,35 @@
+package main.java.lesson5;
+
+import java.util.Scanner;
+
+public class DynamicMatrix {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of rows: ");
+        int rows = scanner.nextInt();
+
+        System.out.print("Enter the number of columns: ");
+        int cols = scanner.nextInt();
+
+        int[][] matrix = new int[rows][cols];
+
+        System.out.println("Enter the matrix elements:");
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] = scanner.nextInt();
+            }
+        }
+
+        System.out.println("Matrix elements: ");
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(matrix[i][j] + "\t");
+            }
+            System.out.println();
+        }
+        scanner.close();
+    }
+}
